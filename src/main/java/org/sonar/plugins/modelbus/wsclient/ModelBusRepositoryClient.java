df69a9e4d82bf375e369ca443ab5b98b02b3521e
Arsenij Solovjev <xeper000@gmail.com>
2012-11-18 14:00:29 +0100

    tell soap client to talk to right wsdl

--- src/main/java/org/sonar/plugins/modelbus/wsclient/ModelBusRepositoryClient.java
+++ src/main/java/org/sonar/plugins/modelbus/wsclient/ModelBusRepositoryClient.java
@@ -1,9 +1,9 @@
 package org.sonar.plugins.modelbus.wsclient;
 
-
 import javax.xml.ws.BindingProvider;
 import javax.xml.namespace.QName;
 
+import java.net.MalformedURLException;
 import java.net.URL;
 import javax.xml.ws.Service;
 import org.slf4j.Logger;
@@ -12,34 +12,42 @@ import org.sonar.plugins.modelbus.wsclient.repository.AuthenticateResponse;
 import org.sonar.plugins.modelbus.wsclient.repository.Repository;
 import org.sonar.plugins.modelbus.wsclient.repository.RepositoryServiceService;
 
+/**
+ * SOAP Client that can talk to the
+ * 
+ * @author arsenij
+ * 
+ */
 public class ModelBusRepositoryClient {
 	Logger logger = LoggerFactory.getLogger(getClass());
-	
-		   public static void main(String[] args) {
-		      if (args.length != 1) {
-		         System.out.println("Usage: WSClient &lt;search item>");
-		         System.exit(-1);
-		      }
 
-		      ModelBusRepositoryClient wsc = new ModelBusRepositoryClient();
-		      wsc.run();
-		   }
+	public static void main(String[] args) {
+		ModelBusRepositoryClient wsc = new ModelBusRepositoryClient();
+		wsc.run();
+	}
 
-		   private void run() {
-		      try {
+	private void run() {
+		try {
 
-		    	  URL wsdlURL = new URL("http://localhost/hello?wsdl");
-		    	  QName SERVICE_NAME = new QName("http://apache.org/hello_world_soap_http", "SOAPService");
-		    	  Service service = RepositoryServiceService.create(wsdlURL, SERVICE_NAME);
-		    	  Repository client = service.getPort(Repository.class);
-		    	  String result = client.authenticate("modelbus", "modelbus");
-		    	  logger.info("My first log: " + result); 
-		    	  System.out.println(result);
-		      } catch (Exception e) {
-		         System.out.println("Exception: " + e.getMessage());
-		      }
-		   }
-		   
-		   
+			Repository client = createClient();
+
+			String result = client.authenticate("modelbus", "modelbus");
+			logger.info("My first log: " + result);
+			System.out.println(result);
+		} catch (Exception e) {
+			System.out.println("Exception: " + e.getMessage());
+		}
+	}
+
+	private Repository createClient() throws MalformedURLException {
+		URL wsdlURL = new URL("http://127.0.1.1:8080/modelbusrepository?wsdl");
+		QName SERVICE_NAME = new QName("http://www.modelbus.org/Repository/",
+				"RepositoryServiceService");
+		@SuppressWarnings("restriction")
+		Service service = RepositoryServiceService.create(wsdlURL, SERVICE_NAME);
+		@SuppressWarnings("restriction")
+		Repository client = service.getPort(Repository.class);
+		return client;
+	}
 
 }
