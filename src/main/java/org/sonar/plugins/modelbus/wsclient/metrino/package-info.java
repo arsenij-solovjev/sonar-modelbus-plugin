d6f3ecda6f55d1539bbeda38eea67aad6fc9b203
Arsenij Solovjev <xeper000@gmail.com>
2013-01-19 20:06:43 +0100

    cleanup

--- src/main/java/org/sonar/plugins/modelbus/wsclient/metrino/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.modelbus.org/MetrinoService/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package org.sonar.plugins.modelbus.wsclient.metrino;
