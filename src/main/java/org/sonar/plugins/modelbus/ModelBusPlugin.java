6fea96a7b48d98cb84758ae2aaec7b5ae10dc77e
Lotes <markus.rudolph1@gmx.de>
2013-01-26 16:22:56 +0100

    modelbus widget

--- src/main/java/org/sonar/plugins/modelbus/ModelBusPlugin.java
+++ src/main/java/org/sonar/plugins/modelbus/ModelBusPlugin.java
@@ -8,7 +8,7 @@ import org.sonar.api.Property;
 import org.sonar.api.SonarPlugin;
 import org.sonar.plugins.modelbus.batch.ModelBusSensor;
 import org.sonar.plugins.modelbus.ui.ExampleFooter;
-import org.sonar.plugins.modelbus.ui.ExampleRubyWidget;
+import org.sonar.plugins.modelbus.ui.ModelBusWidget;
 
 import java.util.Arrays;
 import java.util.List;
@@ -35,6 +35,6 @@ public final class ModelBusPlugin extends SonarPlugin {
         // Batch
         ModelBusSensor.class,
         // UI
-        ExampleFooter.class, ExampleRubyWidget.class);
+        ExampleFooter.class, ModelBusWidget.class);
   }
 }
